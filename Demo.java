db93e4dc469641444c7d555562f2c3510674191b
s0manath <soma756126@gmail.com>
2024-02-07 00:29:34 +0530

    cr-122 updated

--- Demo.java
+++ Demo.java
@@ -3,5 +3,10 @@ public class Demo
            public static void main(String []args)
             {
                 System.out.println("git hub");
+                
             }
+          //CR127
+            public static void doProcess(){
+                 //logic
+              }
          }
